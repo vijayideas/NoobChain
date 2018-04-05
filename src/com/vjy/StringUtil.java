1df7cc22f7cdefd992d9f31b1277c78902312960
Vijay <vijayicfaics@gmail.com>
2018-04-05 16:14:04 +0530

    Initial commit

---
+++ src/com/vjy/StringUtil.java
@@ -0,0 +1,37 @@
+package com.vjy;
+
+import com.google.gson.GsonBuilder;
+
+import java.security.MessageDigest;
+
+public class StringUtil {
+    //Applies Sha256 to a string and returns the result.
+    public static String applySha256(String input){
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            //Applies sha256 to our input,
+            byte[] hash = digest.digest(input.getBytes("UTF-8"));
+            StringBuilder hexString = new StringBuilder(); // This will contain hash as hexidecimal
+            for (byte aHash : hash) {
+                String hex = Integer.toHexString(0xff & aHash);
+                if (hex.length() == 1) hexString.append('0');
+                hexString.append(hex);
+            }
+            return hexString.toString();
+        }
+        catch(Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    //Short hand helper to turn Object into a json string
+    public static String getJson(Object o) {
+        return new GsonBuilder().setPrettyPrinting().create().toJson(o);
+    }
+
+    //Returns difficulty string target, to compare to hash. eg difficulty of 5 will return "00000"
+    public static String getDificultyString(int difficulty) {
+        return new String(new char[difficulty]).replace('\0', '0');
+    }
+
+}
